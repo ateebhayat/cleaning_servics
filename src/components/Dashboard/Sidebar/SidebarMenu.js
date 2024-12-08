6ce9ab6de310efb564a90ee7788af7052f6b7990
ateebhayat <20042147@mydbs.ie>
2024-12-08 22:22:44 +0500

    feat: updated routes in sidebar menus

--- src/components/Dashboard/Sidebar/SidebarMenu.js
+++ src/components/Dashboard/Sidebar/SidebarMenu.js
@@ -1,86 +1,18 @@
 import homeLight from '@icons/home-light.svg';
-import product from '@images/product.png';
 import marketing from '@images/marketing.png';
-import group from '@images/group.png';
-import support from '@images/dollar.png';
-
-export const sideBarAdminItems = [
-  {
-    id: 1,
-    name: 'Dashboard',
-    iconLight: homeLight,
-    linkTo: '/home'
-  },
-  {
-    id: 2,
-    name: 'User Management',
-    iconLight: group,
-    linkTo: '/user_management'
-  },
-  {
-    id: 3,
-    name: 'Campaign Management',
-    iconLight: marketing,
-    linkTo: '/campaign_management'
-  },
-  {
-    id: 4,
-    name: 'Product Management',
-    iconLight: product,
-    linkTo: '/product_management'
-  },
-  {
-    id: 5,
-    name: 'Transactions History',
-    iconLight: support,
-    linkTo: '/transactions_history'
-  }
-];
-
-export const sideBarFundRaiserItems = [
-  {
-    id: 1,
-    name: 'Dashboard',
-    iconLight: homeLight,
-    linkTo: '/fundraise'
-  },
-  {
-    id: 2,
-    name: 'Campaign',
-    iconLight: marketing,
-    linkTo: '/fundraise/campaigns_management'
-  },
-  {
-    id: 3,
-    name: 'Product Management',
-    iconLight: product,
-    linkTo: '/fundraise/products_management'
-  },
-  {
-    id: 4,
-    name: 'Transactions History',
-    iconLight: support,
-    linkTo: '/fundraise/transactions_history'
-  }
-];
 
 export const sideBarBuyItems = [
   {
     id: 1,
     name: 'Dashboard',
     iconLight: homeLight,
-    linkTo: '/seller'
+    linkTo: '/services'
   },
   {
     id: 2,
     name: 'Shop Listing',
     iconLight: marketing,
-    linkTo: '/seller/shop_listing'
+    linkTo: '/services/shop_listing'
   },
-  {
-    id: 3,
-    name: 'Chat',
-    iconLight: support,
-    linkTo: '/sellerchat'
-  }
+
 ];
